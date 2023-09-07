ed1c65553c9f39b5ad57741f01dc39956753db76
camilaleniss <mlmariacami2@gmail.com>
2023-09-06 22:31:01 -0500

    Add interface segregation and dependency inversion principle

--- solid-principles/README.md
+++ solid-principles/README.md
@@ -18,7 +18,16 @@ It's well illustrated using Specification pattern.
 Stands that if there is any inheritance or extension of a class  
 It should continue working and behaving correctly  
 Based on the principles and assumptions it relies on.  
+It's not truly applicable in Go, but we made a case that applies  
 
 ## 4. Interface Segregation Principle
 
+You shouldn't put too much in an interface  
+Makes sense to break into several interfaces  
+So we won't end up having to implement and leaving alone methods that are not neccessary  
+
 ## 5. Dependency Inversion Principle
+
+Dependency inversion != Dependency injection  
+High level modules should not depend on low level modules
+Both should depend on abstractions  
---
+++ solid-principles/dependency-inversion/main.go
@@ -0,0 +1,90 @@
+package main
+
+import "fmt"
+
+// Dependency Inversion Principle
+// high level modules should not depend on low level modules
+// Both should depend on abstractions
+
+type Relationship int
+
+const (
+	Parent Relationship = iota
+	Child
+	Sibling
+)
+
+type Person struct {
+	name string
+	// other useful stuff here
+}
+
+type Info struct {
+	from         *Person
+	relationship Relationship
+	to           *Person
+}
+
+type RelationshipBrowser interface {
+	FindAllChildrenOf(name string) []*Person
+}
+
+type Relationships struct {
+	relations []Info
+}
+
+func (rs *Relationships) FindAllChildrenOf(name string) []*Person {
+	result := make([]*Person, 0)
+
+	for i, v := range rs.relations {
+		if v.relationship == Parent &&
+			v.from.name == name {
+			result = append(result, rs.relations[i].to)
+		}
+	}
+
+	return result
+}
+
+func (rs *Relationships) AddParentAndChild(parent, child *Person) {
+	rs.relations = append(rs.relations,
+		Info{parent, Parent, child})
+	rs.relations = append(rs.relations,
+		Info{child, Child, parent})
+}
+
+type Research struct {
+	// relationships Relationships
+	// if we have that attribute we are relying in a low-level module
+	browser RelationshipBrowser // low-level
+}
+
+func (r *Research) Investigate() {
+	// if there is any change in the way the relation has stored the relations
+	// it breaks everything in a high-level module
+	//relations := r.relationships.relations
+	//for _, rel := range relations {
+	//	if rel.from.name == "John" &&
+	//		rel.relationship == Parent {
+	//		fmt.Println("John has a child called", rel.to.name)
+	//	}
+	//}
+
+	for _, p := range r.browser.FindAllChildrenOf("John") {
+		fmt.Println("John has a child called", p.name)
+	}
+}
+
+func main() {
+	parent := Person{"John"}
+	child1 := Person{"Chris"}
+	child2 := Person{"Matt"}
+
+	// low-level module
+	relationships := Relationships{}
+	relationships.AddParentAndChild(&parent, &child1)
+	relationships.AddParentAndChild(&parent, &child2)
+
+	research := Research{&relationships}
+	research.Investigate()
+}
