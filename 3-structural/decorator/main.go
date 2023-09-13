65461c0a985461431427cf7603782715720d55d5
camilaleniss <mlmariacami2@gmail.com>
2023-09-13 18:36:06 -0500

    Actually add the example for decorator design pattern

--- 3-structural/decorator/main.go
+++ 3-structural/decorator/main.go
@@ -5,3 +5,69 @@ package decorator
 // Not rewriting or altering existent code (Open-close principle)
 // Keep new functionality separate (single responsibility principle)
 // Through embedding
+
+import "fmt"
+
+// our base interface
+type Shape interface {
+	Render() string
+}
+
+type Circle struct {
+	Radius float32
+}
+
+func (c *Circle) Render() string {
+	return fmt.Sprintf("Circle of radius %f",
+		c.Radius)
+}
+
+func (c *Circle) Resize(factor float32) {
+	c.Radius *= factor
+}
+
+type Square struct {
+	Side float32
+}
+
+func (s *Square) Render() string {
+	return fmt.Sprintf("Square with side %f", s.Side)
+}
+
+// possible, but not generic enough
+type ColoredSquare struct {
+	Square
+	Color string
+}
+
+// more generic c:
+type ColoredShape struct {
+	Shape Shape
+	Color string
+}
+
+func (c *ColoredShape) Render() string {
+	return fmt.Sprintf("%s has the color %s",
+		c.Shape.Render(), c.Color)
+}
+
+type TransparentShape struct {
+	Shape        Shape
+	Transparency float32
+}
+
+func (t *TransparentShape) Render() string {
+	return fmt.Sprintf("%s has %f%% transparency",
+		t.Shape.Render(), t.Transparency*100.0)
+}
+
+func main() {
+	circle := Circle{2}
+	fmt.Println(circle.Render())
+
+	redCircle := ColoredShape{&circle, "Red"}
+	fmt.Println(redCircle.Render())
+
+	rhsCircle := TransparentShape{&redCircle, 0.5}
+	fmt.Println(rhsCircle.Render())
+}
